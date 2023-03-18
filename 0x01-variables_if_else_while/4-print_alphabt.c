218cffbd4321af10ab046e173fe28776c9ffa15f
Tajudeen <tajudeentaiwo13@gmail.com>
2023-03-17 17:04:50 -0700

    updated

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i = 0;
 	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z'};
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	while (i < 26)
 	{
 		if (alpha[i] != 'e' && alpha[i] != 'q')
