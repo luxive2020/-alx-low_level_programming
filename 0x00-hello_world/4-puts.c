a4deced88e2a5784b815b17a231f59e332b11aa8
Tajudeen <tajudeentaiwo13@gmail.com>
2023-03-19 06:34:58 -0700

    4-puts.c updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Description: a C program that prints exactly \"Programming
+ * is like building a multilingual puzzle, followed by a new line
+ * Return: 0;
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
