f839ad1cf38dff252c4b89660aead1aeac2345ca
Tajudeen <tajudeentaiwo13@gmail.com>
2023-03-19 08:53:10 -0700

    101-quote added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - main block
  * Description:a C program that prints exactly and that piece
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	fprinf(stderr, "and that piece of art is useful\" - Dora Korpar,
-	2015-10-19\n");
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
